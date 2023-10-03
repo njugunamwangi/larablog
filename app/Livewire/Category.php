fdc8352fc54a5b11d84e05c1d0a256e663ad4073
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-04 01:33:24 +0300

    Sort articles by category

--- app/Livewire/Category.php
+++ app/Livewire/Category.php
@@ -3,11 +3,25 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Article;
 
 class Category extends Component
 {
+    public \App\Models\Category $category;
     public function render()
     {
-        return view('livewire.category');
+        $articles = $this->byCategory();
+
+        return view('livewire.category', compact('articles'));
+    }
+
+    public function byCategory() {
+        return Article::query()
+            ->join('article_category', 'articles.id', '=', 'article_category.article_id')
+            ->where('article_category.category_id', '=', $this->category->id)
+            ->where('status', '=', 'published')
+            ->select('articles.*')
+            ->orderBy('published_at', 'desc')
+            ->paginate(10);
     }
 }
--- resources/views/livewire/category.blade.php
+++ resources/views/livewire/category.blade.php
@@ -1,3 +1,33 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="container mx-auto py-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <!-- Sort by Categories -->
+        <div class="col-span-2">
+
+            <div class="grid grid-cols-2 gap-2 mb-4 justify-between">
+                @foreach($articles as $article)
+                    <article class="flex flex-col shadow my-4">
+                        <!-- Article Image -->
+                        <a href="{{ route('article', $article) }}" class="hover:opacity-75" >
+                            <img src="{{ $article->image() }}" alt="{{ $article->title }}" class="aspect-[4/3] object-contain">
+                        </a>
+                        <div class="bg-white flex flex-col justify-start p-6">
+
+                            <a href="{{ route('article', $article) }}" class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $article->title }}</a>
+                            <p class="text-sm pb-3">
+                                By <a href="{{ route('author', $article->author->slug) }}" class="font-semibold hover:text-gray-800">{{ $article->author->name }}</a>
+                            </p>
+                            <p class="text-sm pb-3">
+                                Published on
+                                {{$article->getFormattedDate()}} | {{ $article->human_read_time }}
+                            </p>
+                            <div class="pb-6">
+                                {{$article->shortBody()}}
+                            </div>
+                            <a href="{{ route('article', $article) }}" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </article>
+                @endforeach
+            </div>
+        </div>
+    </div>
 </div>
