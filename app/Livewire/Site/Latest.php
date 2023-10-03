c19733a59fa5ab630eecbaa621cefa03065e11b0
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-03 19:55:09 +0300

    Latest site section

---
+++ app/Livewire/Site/Latest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\Site;
+
+use App\Models\Article;
+use Livewire\Component;
+
+class Latest extends Component
+{
+    public function render()
+    {
+        $latestArticles = Article::where('status', '=', 'published')
+            ->orderBy('published_at', 'desc')
+            ->limit(2)
+            ->get();
+
+        return view('livewire.site.latest', compact('latestArticles'));
+    }
+}
