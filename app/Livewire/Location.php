2066045c4cc3ceaa762ba451a13f8cedd6d34952
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-04 01:34:18 +0300

    Sort articles by location

--- app/Livewire/Location.php
+++ app/Livewire/Location.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
+use App\Models\Article;
 
 class Location extends Component
 {
+    public \App\Models\Location $location;
     public function render()
     {
-        return view('livewire.location');
+        $articles = $this->byLocations();
+
+        return view('livewire.location', compact('articles'));
+    }
+
+    public function byLocations() {
+        return Article::query()
+            ->join('article_location', 'articles.id', '=', 'article_location.article_id')
+            ->where('article_location.location_id', '=', $this->location->id)
+            ->where('status', '=', 'published')
+            ->select('articles.*')
+            ->orderBy('published_at', 'desc')
+            ->paginate(10);
     }
 }
