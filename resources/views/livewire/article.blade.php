269dca1c1f0bb14f6ec9ca5631bf669f8869628e
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-05 16:35:17 +0300

    refactor article page

--- resources/views/livewire/article.blade.php
+++ resources/views/livewire/article.blade.php
@@ -1,17 +1,44 @@
-
     <!-- Post Section -->
     <section class="w-full md:w-2/3 flex flex-col px-3">
 
         <article class="flex flex-col shadow my-4">
             <!-- Article Image -->
-            <img src="{{ $article->image() }}">
+            <img src="{{ $article->image() }}" class="rounded">
             <div class="bg-white flex flex-col justify-start p-6">
+
                 <p class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $article->title }}</p>
-                <p class="text-sm pb-8">
-                    By <a href="#" class="font-semibold hover:text-gray-800">{{ $article->author->name }}</a>,
-                    Published on {{$article->getFormattedDate()}} | {{ $article->human_read_time }}
-                </p>
-                <div class="sm:prose prose-sm prose">
+
+                <div class="flex justify-between pb-4">
+                    <p class="">
+                        By <a href="{{ route('author', $article->author->slug) }}" class="font-semibold hover:text-gray-800">{{ $article->author->name }}</a>
+                    </p>
+                    <p class="flex flex-row gap-2 items-center m-1">
+                        <x-heroicon-m-calendar class="h-6 w-6 text-blue-600" /> {{$article->getFormattedDate()}}
+                    </p>
+                    <p>
+                        {{ $article->human_read_time }}
+                    </p>
+                </div>
+
+                <div class="flex flex-row gap-2 justify-between">
+                    <div class="flex flex-row gap-2 ">
+                        <x-heroicon-m-hashtag class="h-6 w-6 m-1 text-blue-600" />
+                        @foreach($article->categories as $category)
+                            <a href="{{ route('by-category', $category) }}" class="bg-blue-600 inline-block rounded-full py-1 px-4 text-center text-xs font-semibold leading-loose text-white">
+                                {{$category->category}}
+                            </a>
+                        @endforeach
+                    </div>
+                    <div class="flex flex-row gap-2 ">
+                        <x-heroicon-m-map-pin class="h-6 w-6 m-1 text-lime-700" />
+                        @foreach($article->locations as $location)
+                            <a href="{{ route('by-location', $location) }}" class="bg-lime-700 inline-block rounded-full py-1 px-4 text-center text-xs font-semibold leading-loose text-white">
+                                {{$location->location}}
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="sm:prose prose-sm prose prose-a:text-blue-600">
                     <x-markdown>
                         {!! $article->body !!}
                     </x-markdown>
