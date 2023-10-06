6cf86554da33763e216ea11a4807b09a5feb6904
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-06 19:47:32 +0300

    Add quick create to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/awcodes/filament-quick-create/resources/**/*.blade.php'
     ],
 
     theme: {
