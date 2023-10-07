7e6d650634e09955907796bdf310012e9424aa4e
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-08 01:23:02 +0300

    Top categories

--- app/Livewire/Site.php
+++ app/Livewire/Site.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
+use App\Models\Category;
 
 class Site extends Component
 {
     public function render()
     {
-        return view('livewire.site');
+        $categories = $this->categories();
+
+        return view('livewire.site', compact('categories'));
+    }
+
+    public function categories() {
+        return Category::query()
+            ->with(['articles' => function($query) {
+                $query->orderByDesc('published_at')
+                    ->limit(3);
+            }])
+            ->whereHas('articles', function($query) {
+                $query->where('status', '=', 'published');
+            })
+            ->select('categories.*')
+            ->selectRaw('MAX(articles.published_at) as max_date')
+            ->leftJoin('article_category', 'categories.id', '=', 'article_category.category_id')
+            ->leftJoin('articles', 'articles.id', '=','article_category.article_id')
+            ->orderBy('max_date')
+            ->groupBy('categories.id', 'categories.category', 'categories.slug', 'categories.deleted_at', 'categories.created_at', 'categories.updated_at')
+            ->limit(3)
+            ->get();
     }
 }
--- resources/views/livewire/site.blade.php
+++ resources/views/livewire/site.blade.php
@@ -7,5 +7,32 @@
 
     <!-- Recommended News -->
     <livewire:site.recommended />
+
+    @foreach($categories as $category)
+        <div>
+            <div class="text-lg sm:text-xl font-bold text-blue-500 pb-1 border-b-2 border-blue-500 mb-3 flex justify-between">
+                <div>
+                    <h2 class="uppercase">
+                        {{$category->category}}
+                    </h2>
+                </div>
+                <div class="text-sm">
+                    <a href="{{ route('by-category', $category) }}">
+                        Read more
+                    </a>
+                </div>
+            </div>
+
+            <div>
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
+                    @foreach($category->articles()->limit(3)->get() as $article)
+                        <div>
+                            <livewire:article-item :article="$article" />
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
 
