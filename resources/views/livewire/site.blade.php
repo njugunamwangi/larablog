3d7d86f806c69c007aaf30263dc2adad2a9e87e6
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-08 00:45:08 +0300

    Latest & popular section

--- resources/views/livewire/site.blade.php
+++ resources/views/livewire/site.blade.php
@@ -1,3 +1,10 @@
-<div>
-    @livewire(\App\Livewire\Site\Latest::class)
+<div class="container mx-auto py-6">
+    <!-- Latest & Popular Section -->
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <livewire:site.latest />
+        <livewire:site.popular />
+    </div>
+
+    <!-- Recommended News -->
 </div>
+
