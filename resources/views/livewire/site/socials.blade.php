79af31cdb23236ed26603c82268b9c6deee27fc3
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-13 21:54:24 +0300

    Resize size socials

--- resources/views/livewire/site/socials.blade.php
+++ resources/views/livewire/site/socials.blade.php
@@ -1,11 +1,11 @@
 <div class="flex flex-row justify-between mb-4">
     @foreach($socials as $social)
         @if($social->platform == 'X')
-            <a href="{{ $social->link }}" > <x-fab-twitter class="h-10 w-10" /> </a>
+            <a href="{{ $social->link }}" > <x-fab-twitter class="h-6 w-6" /> </a>
         @elseif($social->platform == 'LinkedIn')
-            <a href="{{ $social->link }}" > <x-fab-linkedin class="h-10 w-10" /> </a>
+            <a href="{{ $social->link }}" > <x-fab-linkedin class="h-6 w-6" /> </a>
         @elseif($social->platform == 'Telegram')
-            <a href="{{ $social->link }}" > <x-fab-telegram class="h-10 w-10" /> </a>
+            <a href="{{ $social->link }}" > <x-fab-telegram class="h-6 w-6" /> </a>
         @endif
     @endforeach
 </div>
