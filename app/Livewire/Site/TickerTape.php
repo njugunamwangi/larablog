586cd6a5e8bf39c86eeef25d840fd0d600b89088
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-16 23:22:55 +0300

    Ticker tape update

---
+++ app/Livewire/Site/TickerTape.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Site;
+
+use Livewire\Component;
+
+class TickerTape extends Component
+{
+    public function render()
+    {
+        return view('livewire.site.ticker-tape');
+    }
+}
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,43 +34,7 @@
 
         <livewire:site.category />
 
-        <!-- TradingView Widget BEGIN -->
-        <div class="tradingview-widget-container">
-            <div class="tradingview-widget-container__widget"></div>
-            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
-            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
-                {
-                    "symbols": [
-                    {
-                        "proName": "FOREXCOM:SPXUSD",
-                        "title": "S&P 500"
-                    },
-                    {
-                        "proName": "FOREXCOM:NSXUSD",
-                        "title": "US 100"
-                    },
-                    {
-                        "proName": "FX_IDC:EURUSD",
-                        "title": "EUR to USD"
-                    },
-                    {
-                        "proName": "BITSTAMP:BTCUSD",
-                        "title": "Bitcoin"
-                    },
-                    {
-                        "proName": "BITSTAMP:ETHUSD",
-                        "title": "Ethereum"
-                    }
-                ],
-                    "showSymbolLogo": true,
-                    "colorTheme": "light",
-                    "isTransparent": false,
-                    "displayMode": "adaptive",
-                    "locale": "en"
-                }
-            </script>
-        </div>
-        <!-- TradingView Widget END -->
+        <livewire:site.ticker-tape />
 
 
         <div class="container mx-auto flex flex-wrap py-6">
