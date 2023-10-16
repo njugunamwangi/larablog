2bbc09cdcfd00539e844404a50485de9303e26df
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-16 20:24:16 +0300

    Tape ticker

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,6 +34,44 @@
 
         <livewire:site.category />
 
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+            <div class="tradingview-widget-container__widget"></div>
+            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+            <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+                {
+                    "symbols": [
+                    {
+                        "proName": "FOREXCOM:SPXUSD",
+                        "title": "S&P 500"
+                    },
+                    {
+                        "proName": "FOREXCOM:NSXUSD",
+                        "title": "US 100"
+                    },
+                    {
+                        "proName": "FX_IDC:EURUSD",
+                        "title": "EUR to USD"
+                    },
+                    {
+                        "proName": "BITSTAMP:BTCUSD",
+                        "title": "Bitcoin"
+                    },
+                    {
+                        "proName": "BITSTAMP:ETHUSD",
+                        "title": "Ethereum"
+                    }
+                ],
+                    "showSymbolLogo": true,
+                    "colorTheme": "light",
+                    "isTransparent": false,
+                    "displayMode": "adaptive",
+                    "locale": "en"
+                }
+            </script>
+        </div>
+        <!-- TradingView Widget END -->
+
 
         <div class="container mx-auto flex flex-wrap py-6">
 
@@ -42,6 +80,7 @@
         </div>
 
         <footer class="w-full border-t bg-white pb-12">
+
             <div class="w-full container mx-auto flex flex-col items-center">
                 <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
                     <a href="{{ route('about-us') }}" class="uppercase px-3">About Us</a>
