4abb5a8e7109a7d2bf35eca34dfba762ca4a127e
njugunamwangi <njugunamwangi.n@gmail.com>
2023-10-16 18:54:16 +0300

    Popular articles

--- app/Livewire/Site/Popular.php
+++ app/Livewire/Site/Popular.php
@@ -3,14 +3,39 @@
 namespace App\Livewire\Site;
 
 use App\Models\Article;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Popular extends Component
 {
     public function render()
     {
-        $popularArticles = Article::where('status', '=', 'published')
-            ->orderBy('published_at', 'desc')
+        $popularArticles = Article::query()
+            ->leftJoin('upvote_downvotes', 'articles.id', '=', 'upvote_downvotes.article_id')
+            ->select('articles.*', DB::raw('COUNT(upvote_downvotes.id) as upvote_count'))
+            ->where(function($query) {
+                $query->whereNull('upvote_downvotes.is_upvote')
+                    ->orWhere('upvote_downvotes.is_upvote', '=', 1);
+            })
+            ->where('status', '=', 'published')
+            ->orderByDesc('upvote_count')
+            ->groupBy(
+                'articles.id',
+                'articles.title',
+                'articles.slug',
+                'articles.body',
+                'articles.status',
+                'articles.published_at',
+                'articles.scheduled_for',
+                'articles.author_id',
+                'articles.editor_id',
+                'articles.meta_title',
+                'articles.meta_description',
+                'articles.deleted_at',
+                'articles.created_at',
+                'articles.updated_at',
+                'articles.ancient_truth',
+            )
             ->limit(6)
             ->get();
 
