b1352e8ba78fbcb2a928e32a242962b9023c5499
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-24 23:43:32 +0300

    Location seeder

---
+++ database/factories/LocationFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Location>
+ */
+class LocationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'location' => $location = fake()->country(),
+            'slug' => Str::slug($location)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(EditorSeeder::class);
         $this->call(AuthorSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(LocationSeeder::class);
     }
 }
