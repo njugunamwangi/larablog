74d515618b3ea3cfa7e782241963028ed0e79e82
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-25 21:39:25 +0300

    Fix site pages

--- resources/views/livewire/site/about.blade.php
+++ resources/views/livewire/site/about.blade.php
@@ -2,7 +2,7 @@
 
     <article class="flex flex-col shadow my-4">
         <a href="#" class="hover:opacity-75">
-            <img src="{{ $widget->getMedia('text-widgets')->first()->getUrl() }}">
+            <img src="{{ $widget->getMedia('text-widgets')->isEmpty() ? "https://placehold.co/600x400?text=" . 'About Us' : $widget->getMedia('text-widgets')->first()->getUrl() }}">
         </a>
         <div class="bg-white flex flex-col justify-start p-6">
             <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">{{ $widget->title }}</a>
